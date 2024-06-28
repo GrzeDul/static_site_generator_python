8bac93899eb91cbde25fcced4c2f100222cf6f0e
GrzeDul <grzesta93@gmail.com>
2024-06-28 19:14:59 +0200

    Add extract markdown and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -10,7 +10,6 @@ from textnode import (
     text_type_code,
     text_type_image,
     text_type_link,
-    split_nodes_delimiter,
 )
 
 class TestTextNode(unittest.TestCase):
@@ -41,32 +40,6 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
-    def test_split_nodes_delimeter(self):
-        node1 = TextNode("*This* is text with a *bold block1* word", text_type_text)
-        node2 = TextNode("That *bold block2**", text_type_text)
-        node3 = TextNode("*bold block3* here", text_type_text)
-        node4 = TextNode("*bold block4*", text_type_text)
-        node5 = TextNode("just text4*", text_type_text)
-        node6 = TextNode("just text5", text_type_text)
-        node7 = TextNode("just text6", text_type_text)
-        new_nodes = split_nodes_delimiter([node1, node2, node3, node4,  node5, node6, node7], "*", text_type_bold)
-        self.assertEqual(new_nodes, 
-        [
-        TextNode("This" , "bold", None),
-        TextNode(" is text with a " , "text", None),
-        TextNode("bold block1", "bold", None),
-        TextNode(" wordThat ", "text", None),
-        TextNode("bold block2","bold", None),
-        TextNode("bold block3","bold", None),
-        TextNode(" here", "text", None),
-        TextNode("bold block4","bold", None),
-        TextNode("just text4","text", None),
-        TextNode("just text5just text6","text", None)]
-        )
-    def test_split_node_empty(self):
-        node = TextNode("****text***", text_type_text)
-        new_nodes = split_nodes_delimiter([node],"*",text_type_bold)
-        self.assertEqual(new_nodes, [TextNode("text", "bold", None)])
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -36,35 +36,3 @@ def text_node_to_html_node(text_node):
         return LeafNode("img",value="", props = {"src":text_node.src, "alt":text_node.value})
     else:
         raise ValueError(f"invalid text_type: {text_node.text_type}")
-
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    old_nodes_text = ""
-    for node in old_nodes:
-        old_nodes_text += node.text
-    new_nodes_text = old_nodes_text.split(delimiter)
-    print(new_nodes_text)
-    new_nodes = []
-    bold = False
-    for node in new_nodes_text:
-        if node == '':
-            bold = True
-            continue
-        if bold and node != new_nodes_text[-1]:
-            new_node = TextNode( text = node, text_type = text_type)
-            new_nodes.append(new_node)
-            bold = False
-        else:
-            new_node = TextNode(text = node,text_type = text_type_text)
-            new_nodes.append(new_node)
-            bold = True
-    return new_nodes
-
-node1 = TextNode("This is text with a *bold block1* word", text_type_text)
-node2 = TextNode("That *bold block2**", text_type_text)
-node3 = TextNode("*bold block3* was", text_type_text)
-node4 = TextNode("*bold block4*", text_type_text)
-node5 = TextNode("just text4*", text_type_text)
-node6 = TextNode("just text5", text_type_text)
-node7 = TextNode("just text6", text_type_text)
-new_nodes = split_nodes_delimiter([node1, node2, node3, node4,  node5, node6, node7], "*", text_type_bold)
-print(new_nodes)
